fc0560f2374e682cfb6db33ef20568bb209cc5fd
ansorokey <ansorokey@gmail.com>
2023-11-15 19:44:22 -0500

    completed built-in functions

--- index.py
+++ index.py
@@ -1,16 +1,45 @@
-from string import Template
+import itertools
 
-str_templ = Template("${name} chased squirrels at the ${place}")
+# the cycle method iterates in a loop endlessly
+seq1 = ['Dante', 'Vergil', 'Nero']
+iter1 = itertools.cycle(seq1)
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
 
-str1 = str_templ.substitute(name="Luna", place="park")
-print(str1)
+# the count creates a counter with a starting and step value
+count1 = itertools.count()
+print(next(count1))
+print(next(count1))
+print(next(count1))
 
-# Since the substition uses keyword arguments, we can pass in a dictionary instead
+# the accumulate aggregates values together,
+# defaults to addition but the function can be changed
+vls = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+acc = itertools.accumulate(vls, max)
+print(next(acc))
+print(next(acc))
+print(next(acc))
 
-data = {
-    "name": "Luna",
-    "place":  "park"
-}
 
-str2 = str_templ.substitute(data)
-print(str2)
+# the chain connects sequences together
+abcs = 'abc'
+one_two_threes = '123'
+chain = itertools.chain(abcs, one_two_threes)
+print(list(chain))
+
+
+# dropwhile and takewhile will return values until a condition is met
+# takewhile will return values until it fails a condition
+# dropwhile will ignore values until it passes a test
+# passing or failing a test after doesnt matter
+vls = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+def bigger_than_40(x):
+    return x < 40
+
+take = list(itertools.takewhile(bigger_than_40, vls))
+print(take)
+drop = list(itertools.dropwhile(bigger_than_40, vls))
+print(drop)
